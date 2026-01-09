a5c9288e476ebcedad7137ce2d7e027af7b6b3e1
Anshuman KC <anshumankc20@gmail.com>
2026-01-09 18:07:53 +0545

    Revise conditional basics (if else clauses)

--- basics.js
+++ basics.js
@@ -1,4 +1,5 @@
 console.log("I am learning JavaScript!")
+// Revising objects and their uses
 let player = {
   name = "Frog ",
   age = 10
@@ -7,4 +8,18 @@ let player = {
 
 function logData() {
   console.log(player.name + "is " + player.age + " years old ")
+}
+// revising conditional statements
+let age = 15
+if (age < 6) {
+    console.log("free")
+} else if (age >= 6 && age <= 17) {
+    console.log("child discount")
+} else if (age <= 18 && age >=26) {
+    console.log("student discount")
+} else if (age <= 27 && age >= 66) {
+    console.log("full price")
+    
+} else {
+    console.log("Senior citizen discount")
 }
