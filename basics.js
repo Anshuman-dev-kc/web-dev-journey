63ac981f55d782002fd607ed796f7fe66e9f7c6e
Anshuman KC <anshumankc20@gmail.com>
2026-01-09 18:00:42 +0545

    Practice creating an object and combine them in a string

--- basics.js
+++ basics.js
@@ -1,1 +1,10 @@
 console.log("I am learning JavaScript!")
+let player = {
+  name = "Frog ",
+  age = 10
+  IsHuman = false
+}
+
+function logData() {
+  console.log(player.name + "is " + player.age + " years old ")
+}
