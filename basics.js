8c07fd1cba3bcf302111fe0abfd7e928b06c65c9
Anshuman KC <anshumankc20@gmail.com>
2026-01-09 18:29:44 +0545

    PRACTICE pop push shift and unshift in arrays

--- basics.js
+++ basics.js
@@ -30,3 +30,13 @@ let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
 for (let i = 0; i < largeCountries.length; i++ ) {
     console.log(largeCountries[i])
 }
+// learning push pop shift unshift
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+largeCountries.pop()
+largeCountries.push("Pakistan")
+largeCountries.shift()
+largeCountries.unshift("China")
+for (let i = 0; i < largeCountries.length; i++){
+    console.log(largeCountries[i])
+}
